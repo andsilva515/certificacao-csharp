004c9aa1c6803fb1c0c368ce447e2d287665e805
andsilva515 <anderson.ssilva@outlook.com>
2024-10-30 09:42:13 -0300

    lógica de decisão com instruções if

--- CsharpProjects/TestProjectQuatro/obj/Debug/net8.0/TestProjectQuatro.AssemblyInfo.cs
+++ CsharpProjects/TestProjectQuatro/obj/Debug/net8.0/TestProjectQuatro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjectQuatro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d5d1c2ad3cac84aeec6ae398e0078defc51187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb5480e75c9c077245222c2496d545ccbfc432e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjectQuatro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjectQuatro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
